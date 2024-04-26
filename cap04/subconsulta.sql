508d02381a7e82adaff6edf4a70adb2aa7c99fc1
valdenio458 <valdeniomartinho@gmail.com>
2024-04-26 10:31:12 -0300

    Subconsulta

---
+++ cap04/subconsulta.sql
@@ -0,0 +1,11 @@
+SELECT 
+  account_id,
+  product_cd,
+  cust_id,
+  avail_balance
+FROM account 
+WHERE product_cd IN (
+  SELECT product_cd
+  FROM product
+  WHERE product_type_cd = 'ACCOUNT'
+);
