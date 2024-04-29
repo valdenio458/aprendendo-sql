0414c0b26607abdec123d08a89dbebf35f3d2585
valdenio458 <valdeniomartinho@gmail.com>
2024-04-29 14:41:53 -0300

    Exercício 6.1

---
+++ cap06/exercicio-6.1.sql
@@ -0,0 +1,13 @@
+-- Se o conjunto A = {L M N O P} e o conjunto B = {P Q R S T}, quais os conjuntos são gerados pelas seguintes operações?
+
+A union B
+L M N O P Q R S T 
+
+A union all B
+L M N O P P Q R S T 
+
+A intersect B
+P 
+
+A except B
+L M N O 
