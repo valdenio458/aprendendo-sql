7b99326650e16bce1f7f1857e594aad4508b9493
valdenio458 <valdeniomartinho@gmail.com>
2024-05-09 10:15:00 -0300

    OUTER JOIN (LEFT) com comentário

--- cap10/outer_join_2.sql
+++ cap10/outer_join_2.sql
@@ -5,3 +5,18 @@ FROM customer c LEFT OUTER JOIN business b
 ON c.cust_id = b.cust_id
 
 
+-- Resultado:
+-- cust_id	name
+-- 1	null
+-- 2	null
+-- 3	null
+-- 4	null
+-- 5	null
+-- 6	null
+-- 7	null
+-- 8	null
+-- 9	null
+-- 10	Chilton Engineering
+-- 11	Northeast Cooling Inc.
+-- 12	Superior Auto Body
+-- 13	AAA Insurance Inc.
