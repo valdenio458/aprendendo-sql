bfc3524f10d364fd0d0bf47fd1489afce7d085dc
valdenio458 <valdeniomartinho@gmail.com>
2024-05-13 15:43:10 -0300

    Exercício 10.2

---
+++ cap10/exercicio-10.2.sql
@@ -0,0 +1,8 @@
+-- Escreva uma consulta que retorne todos os nomes de produtos junto com as contas baseadas naquele produto (use a coluna 'product_cd' da tabela 'account' para vinculá-la à tabela 'product'). Inclua todos os produtos, mesmo que nenhuma conta tenha sido aberta para aquele produto.
+SELECT
+  p.product_cd,
+  a.account_id,
+  a.cust_id,
+  a.avail_balance
+FROM account a RIGHT OUTER JOIN product p
+ON p.product_cd = a.product_cd;
