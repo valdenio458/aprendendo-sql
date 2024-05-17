96c66438d8cfe3ff8cfadedd677e3761a935f493
valdenio458 <valdeniomartinho@gmail.com>
2024-05-17 10:40:23 -0300

    Cria a 'view' business_customer_vw

---
+++ cap14/view_5.sql
@@ -0,0 +1,18 @@
+CREATE VIEW business_customer_vw
+(cust_id,
+fed_id,
+cust_type_cd,
+address,
+city,
+state,
+zipcode)
+AS 
+SELECT 
+  cust_id,
+  concat('ends in ', substr(fed_id, 8, 4)) fed_id,
+  cust_type_cd,
+  address,
+  city,
+  state,
+  postal_code
+  FROM customer;
